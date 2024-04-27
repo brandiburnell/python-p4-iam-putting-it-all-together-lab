cc619a64856312b5313c0890ed3e6dee90064c9a
Brandi Burnell <brandiburnell6@gmail.com>
2024-04-27 14:44:21 -0600

    add signup feature

--- server/models.py
+++ server/models.py
@@ -1,29 +1,71 @@
+# from sqlalchemy.ext.hybrid import hybrid_property
+# from sqlalchemy_serializer import SerializerMixin
+
+# from config import db, bcrypt
+
+
+# class User(db.Model, SerializerMixin):
+#     __tablename__ = 'users'
+
+#     serialize_rules = ('-recipes.user', '-_password_hash',)
+
+#     id = db.Column(db.Integer, primary_key = True)
+#     username = db.Column(db.String, unique=True, nullable=False)
+#     _password_hash = db.Column(db.String)
+#     image_url = db.Column(db.String)
+#     bio = db.Column(db.String)
+
+#     recipes = db.relationship('Recipe', backref='users')
+
+#     @hybrid_property
+#     def password_hash(self):
+#         raise AttributeError('You cannot see user passwords')
+    
+#     @password_hash.setter
+#     def password_hash(self, password):
+#         print(password)
+#         password_hash = bcrypt.generate_password_hash(
+#             password.encode('utf-8'))
+#         self._password_hash = password_hash.decode('utf-8')
+
+#     def authenticate(self, password):
+#         return bcrypt.check_password_hash(
+#             self._password_hash, password.encode('utf-8'))
+
+# class Recipe(db.Model, SerializerMixin):
+#     __tablename__ = 'recipes'
+
+#     id = db.Column(db.Integer, primary_key=True)
+#     title = db.Column(db.String, nullable=False)
+#     minutes_to_complete = db.Column(db.Integer)
+#     instructions = db.Column(db.String, db.CheckConstraint('length(instructions) >= 50'), nullable=False)
+
+#     user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+
 from sqlalchemy.ext.hybrid import hybrid_property
 from sqlalchemy_serializer import SerializerMixin
 
 from config import db, bcrypt
 
-
 class User(db.Model, SerializerMixin):
     __tablename__ = 'users'
 
-    # serialize_rules = ('-recipes.user', '-_password_hash',)
+    serialize_rules = ('-recipes.user', '-_password_hash',)
 
-    id = db.Column(db.Integer, primary_key = True)
+    id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String, unique=True, nullable=False)
     _password_hash = db.Column(db.String)
     image_url = db.Column(db.String)
     bio = db.Column(db.String)
 
-    recipes = db.relationship('Recipe', backref='users')
+    recipes = db.relationship('Recipe', backref='user')
 
     @hybrid_property
     def password_hash(self):
-        raise AttributeError('You cannot see user passwords')
-    
+        raise AttributeError('Password hashes may not be viewed.')
+
     @password_hash.setter
     def password_hash(self, password):
-        print(password)
         password_hash = bcrypt.generate_password_hash(
             password.encode('utf-8'))
         self._password_hash = password_hash.decode('utf-8')
@@ -32,12 +74,21 @@ class User(db.Model, SerializerMixin):
         return bcrypt.check_password_hash(
             self._password_hash, password.encode('utf-8'))
 
+    def __repr__(self):
+        return f'<User {self.username}>'
+
 class Recipe(db.Model, SerializerMixin):
     __tablename__ = 'recipes'
+    __table_args__ = (
+        db.CheckConstraint('length(instructions) >= 50'),
+    )
 
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String, nullable=False)
+    instructions = db.Column(db.String, nullable=False)
     minutes_to_complete = db.Column(db.Integer)
-    instructions = db.Column(db.String, db.CheckConstraint('length(instructions) >= 50'), nullable=False)
 
-    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    user_id = db.Column(db.Integer(), db.ForeignKey('users.id'))
+
+    def __repr__(self):
+        return f'<Recipe {self.id}: {self.title}>'
